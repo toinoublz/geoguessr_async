acaad0ae0a5f0bbbc9d4bfb14a680124274511d8
toinoublz <antoine.blaise34@gmail.com>
2023-09-23 10:13:47 +0200

    remove old folders

--- src/geoguessr_async/geo_utils.py
+++
@@ -1,9 +0,0 @@
-def flatten_dict(d: dict, parent_key: str='', separator: str=''):
-    items = []
-    for k, v in d.items():
-        new_key = f"{parent_key}{separator}{k.capitalize()}" if parent_key else k
-        if isinstance(v, dict):
-            items.extend(flatten_dict(v, new_key, separator).items())
-        else:
-            items.append((new_key, v))
-    return dict(items)
