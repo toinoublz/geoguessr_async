43ad9fc7c899df63cb96685f48c32a8dfaa68ed4
Antoine BLAISE <antoine.blaise34@gmail.com>
2026-02-15 22:38:39 +0100

    Improve objects [WIP]

--- geoguessr_async/__init__.py
+++ geoguessr_async/__init__.py
@@ -1,1 +1,8 @@
-from .geoguessr import *
+import os
+import sys
+
+from geoguessr_async.geoguessr import *
+
+# Add the parent directory to the Python path
+# This allows importing the geoguessr_async module in tests
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
